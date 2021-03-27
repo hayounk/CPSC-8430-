0881362e5d9df08d78a5897c578a82cae0394f41
hayounk <71373398+hayounk@users.noreply.github.com>
2021-03-27 17:51:00 -0400

    Update hw2_seq2seq.sh

--- HW2/hw2_1/hw2_seq2seq.sh
+++ HW2/hw2_1/hw2_seq2seq.sh
@@ -1,1 +1,6 @@
-python seq2seq.py $1 $2
+#!/bin/bash
+wget https://drive.google.com/u/0/uc?id=1RevHMfXZ1zYjUm4fPU1CfFKAjyMJjdgJ&export=download
+
+tar zxvf MLDS_hw2_1_data.tar.gz
+
+python seq2seq.py $1 $2 --test
